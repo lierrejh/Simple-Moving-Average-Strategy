ff274be205e6214a553a088ff3ee6a11ec0d5ce0
Lierre Henry <93233869+lierrejh@users.noreply.github.com>
2025-07-19 23:21:51 +0100

    Strategy finished 1:2 RR positions

--- strategy.py
+++ strategy.py
@@ -5,12 +5,18 @@ import backtrader.feeds as btfeeds
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-data = btfeeds.PandasData(dataname=yf.download('SPY', start='2024-01-01', end='2025-01-01',multi_level_index=False), plot = True)
+data = btfeeds.PandasData(dataname=yf.download('SPY', start='2010-01-01', end='2025-01-01',multi_level_index=False), plot = True)
 
 cerebro = bt.Cerebro()
 cerebro.adddata(data)
 
 class MyStrategy(bt.Strategy):
+    
+    params = dict(
+        risk_per_trade=0.01,
+        stop_loss=0.01,
+        take_profit=0.02,
+    ) 
 
     def __init__(self):
         self.sma50 = bt.indicators.SimpleMovingAverage(self.data.close, period=50)
@@ -19,18 +25,38 @@ class MyStrategy(bt.Strategy):
         self.sma200.plotinfo.plot = True
     
     def next(self):
+
+        price = self.data.close[0]
+
+
         if not self.position:
             if self.sma50 > self.sma200:
-                self.buy(size=1, exectype=bt.Order.StopTrail, trailamount=0.25)
+                sl_price= price * (1 - self.params.stop_loss)
+                tp_price = price * (1 + self.params.take_profit)
+                size = min((int((1000) / (price - sl_price))), 200)
+                self.buy_bracket(size=size, stopprice=sl_price, limitprice=tp_price)
+
         else:
             if self.sma50 < self.sma200:
-                self.sell(size=1, exectype=bt.Order.StopTrail, trailamount=0.25)
+                sl_price = price * (1 + self.params.stop_loss)
+                tp_price = price * (1 - self.params.take_profit)
+                size = min((int((1000) / (sl_price - price)), 200))
+
+                self.sell_bracket(size=size, stopprice=sl_price, limitprice=tp_price)
 
 cerebro.addstrategy(MyStrategy)
 cerebro.broker.setcash(100000)
+cerebro.addanalyzer(bt.analyzers.SharpeRatio_A, _name='sharpe')
+cerebro.addanalyzer(bt.analyzers.DrawDown, _name='drawdown')
+cerebro.addanalyzer(bt.analyzers.TradeAnalyzer)
+
 result = cerebro.run()
+strat = result[0]
+print("Sharpe Ratio:", strat.analyzers.sharpe.get_analysis())
+print("Drawdown:", strat.analyzers.drawdown.get_analysis())
+
 cerebro.plot(style='candlestick')
-plt.show()
+
 
 
 
